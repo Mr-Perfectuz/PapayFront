04cb50fb22c024fc7e30e3099ac4c2532476b2ff
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2023-11-09 11:39:16 +0900

    checking react router dom

---
+++ src/app/components/dishes.tsx
@@ -0,0 +1,10 @@
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+
+function Dishes() {
+  return (
+    <h2>
+      <Container>Dishes</Container>
+    </h2>
+  );
+}
+export default Dishes;
---
+++ src/app/components/users.tsx
@@ -0,0 +1,10 @@
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+
+function Users() {
+  return (
+    <h2>
+      <Container>Users</Container>
+    </h2>
+  );
+}
+export default Users;
