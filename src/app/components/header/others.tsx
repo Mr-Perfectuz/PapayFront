ccd0c38659fa37908c6460ae803b78dfa3abf86b
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2023-11-12 19:51:18 +0900

    feat: develop header logic of restaurant and others navbar

--- src/app/components/header/others.tsx
+++ src/app/components/header/others.tsx
@@ -1,5 +1,90 @@
+import {
+  Badge,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Stack,
+} from "@mui/material";
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function NavbarOthers(props: any) {
-  return <div>NavbarOthers</div>;
+  return (
+    <div className="home_navbar format_others">
+      <Container>
+        <Stack
+          className="navbar_config"
+          flexDirection={"row"}
+          justifyContent={"space-between"}
+        >
+          <Box>
+            <img src="/icons/papay.svg" alt="header image" />
+          </Box>
+          <Stack
+            display={"flex"}
+            flexDirection={"row"}
+            justifyContent={"space-evenly"}
+            alignItems={"center"}
+            className="navbar_links"
+          >
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/">Bosh Sahifa</NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/restaurant" activeClassName="underline">
+                Oshxona
+              </NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/community" activeClassName="underline">
+                Jamiyat
+              </NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/orders" activeClassName="underline">
+                Buyurtma
+              </NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/help" activeClassName="underline">
+                Yordam
+              </NavLink>
+            </Box>
+            <Box className="hover-line">
+              <IconButton
+                aria-label="card"
+                id="basic-button"
+                aria-controls={undefined}
+                aria-haspopup="true"
+                aria-expanded={undefined}
+                // onClick={handleClick}
+              >
+                <Badge badgeContent={3} color="secondary">
+                  <img
+                    src="/icons/shopping-cart.svg"
+                    alt="shopping card icon"
+                  />
+                </Badge>
+              </IconButton>
+            </Box>
+            <Box>
+              <Button
+                variant="contained"
+                style={{
+                  width: "77px",
+                  height: "36px",
+                  background: "#1976D2",
+                  borderRadius: "4px",
+                  color: "#fff",
+                }}
+              >
+                KIRISH
+              </Button>
+            </Box>
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
