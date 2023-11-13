a70416a046897815666c45aff7be6b56c1a5524c
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2023-11-13 23:21:44 +0900

    develop footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "../css/App.css";
 import "../css/navbar.css";
+import "../css/footer.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import RestaurantPage from "./screens/RestaurantPage";
@@ -13,6 +14,7 @@ import HomePage from "./screens/HomePage";
 import NavbarHome from "./components/header";
 import NavbarRestaurant from "./components/header/restaurant";
 import NavbarOthers from "./components/header/others";
+import Footer from "./components/footer";
 
 function App() {
   const [path, setPath] = useState();
@@ -76,6 +78,8 @@ function App() {
           <HomePage />
         </Route>
       </Switch>
+
+      <Footer />
     </Router>
   );
 }
---
+++ src/app/components/footer/index.tsx
@@ -0,0 +1,74 @@
+import { Box, Container, Stack } from "@mui/material";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer_config">
+      <Container>
+        <Stack className="main_footer_component">
+          <Stack flexDirection={"row"} style={{ height: "242px" }}>
+            <Stack>
+              <Box>
+                <img src="/icons/footer.svg" alt="footer img" />
+              </Box>
+              <Box className="footer_text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor Sed ut perspiciatis unde omnis iste
+              </Box>
+              <Stack sx={{ flexDirection: "row" }} className="footer_icons">
+                <Box>
+                  <img src="/icons/facebook.svg" alt="footer img" />
+                </Box>
+                <Box>
+                  <img src="/icons/twitter.svg" alt="footer img" />
+                </Box>
+                <Box>
+                  <img src="/icons/instagram.svg" alt="footer img" />
+                </Box>
+                <Box>
+                  <img src="/icons/youtube.svg" alt="footer img" />
+                </Box>
+              </Stack>
+            </Stack>
+            <Stack className="center_parts">
+              <Box className="center_parts_one">Bo’limlar</Box>
+              <Box className="center_parts_second"></Box>
+              <Box className="center_parts_third">
+                Bosh Sahifa Oshxonalar Jamiyat <br />
+                Yordam
+              </Box>
+            </Stack>
+            <Stack className="findUs">
+              <Box className="findUs_1"> Bizni top</Box>
+              <Box className="center_parts_second"></Box>
+              <Stack
+                className="findUs_wrapper findUs_wrapper_first"
+                flexDirection={"row"}
+              >
+                <Box className="findUs_contact">L.</Box>
+                <Box className="findUs_contact_text">Uzbekist an</Box>
+              </Stack>
+              <Stack
+                className="findUs_wrapper findUs_wrapper_second"
+                flexDirection={"row"}
+              >
+                <Box className="findUs_contact">P.</Box>
+                <Box className="findUs_contact_text">+998 - 99 266 25 62</Box>
+              </Stack>
+              <Stack className="findUs_wrapper" flexDirection={"row"}>
+                <Box className="findUs_contact">E.</Box>
+                <Box className="findUs_contact_text">Papays@restaurant.com</Box>
+              </Stack>
+              <Box></Box>
+              <Box></Box>
+            </Stack>
+          </Stack>
+          <Box className="liner"></Box>
+          <Box className="copyrights">
+            Copyright Papays 2022, All right reserved.
+          </Box>
+        </Stack>
+      </Container>
+    </div>
+  );
+}
