e398c4444548334940d7935928a44c4bd59ef8e5
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2023-11-22 22:13:06 +0900

    feat: update chosen dish

--- src/app/screens/RestaurantPage/chosenDish.tsx
+++ src/app/screens/RestaurantPage/chosenDish.tsx
@@ -1,5 +1,15 @@
+import { Container, Stack } from "@mui/material";
 import React from "react";
 
 export default function ChosenDish() {
-  return <div>ChosenDish</div>;
+  const lebel = { inputProps: { "aria-label": "Checkbox demo" } };
+
+  return (
+    <div className="chosen_dish_page">
+      <Container className="dish_container">
+        <Stack className="chosen_dish_slider"></Stack>
+        <Stack className="chosen_dish_slider"></Stack>
+      </Container>
+    </div>
+  );
 }
