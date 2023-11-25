85ff416a4ffdccf5c19b136b8ecbee30be6b70ce
Mr-Perfectuz <ziyodovanvar1999@example.com>
2023-11-25 15:14:46 +0900

    develop orders page a

--- src/app/screens/OrdersPage/index.tsx
+++ src/app/screens/OrdersPage/index.tsx
@@ -1,6 +1,25 @@
 import React from "react";
-import { Container } from "@mui/material";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { Box, Container } from "@mui/material";
 
 export default function OrdersPage() {
-  return <Container>OrdersPage</Container>;
+  const [value, setValue] = React.useState(2);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+  return (
+    <div className="order_page">
+      <Container>
+        <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
+          <Tabs value={value} onChange={handleChange} centered>
+            <Tab label="Item One" />
+            <Tab label="Item Two" />
+            <Tab label="Item Three" />
+          </Tabs>
+        </Box>
+      </Container>
+    </div>
+  );
 }
