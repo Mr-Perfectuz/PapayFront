86c15fa081ef02f27195ddca6700a66d8025d90e
Mr-Perfectuz <ziyodovanvar1999@example.com>
2023-12-21 15:19:20 +0900

    feat: homePage screen slice and selector

--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Statistics from "./statistics";
 import TopRestaurants from "./topRestaurants";
 import BestDishes from "./BestDishes";
@@ -7,7 +7,13 @@ import Events from "./events";
 import Recomendations from "./recomendations";
 import "../../../css/home.css";
 import Advertisement from "./advertisements";
+
 export default function HomePage() {
+  // selector : store = > data
+
+  useEffect(() => {});
+  // selector : data = > store
+
   return (
     <div className="homePage">
       <Statistics />
---
+++ src/app/screens/HomePage/selector.ts
@@ -0,0 +1,28 @@
+import { createSelector } from "reselect";
+import { AppRootState } from "../../../types/screen";
+
+const selectHomePage = (state: AppRootState) => state.homePage;
+export const retreiveTopRestaurants = createSelector(
+    selectHomePage,
+    (HomePage) => HomePage.topRestaurants
+),
+export const retreivebestRestaurants = createSelector(
+    selectHomePage,
+    (HomePage) => HomePage.bestRestaurants
+),
+export const retreivetrendProducts = createSelector(
+    selectHomePage,
+    (HomePage) => HomePage.trendProducts
+),
+export const retreivebestBoArticles = createSelector(
+    selectHomePage,
+    (HomePage) => HomePage.bestBoArticles
+),
+export const retreivetrendBoArticles = createSelector(
+    selectHomePage,
+    (HomePage) => HomePage.trendBoArticles
+),
+export const retreivenewsBoArticles = createSelector(
+    selectHomePage,
+    (HomePage) => HomePage.newsBoArticles
+),
